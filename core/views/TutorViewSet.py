ef978c627649a9ad84ba579cbd4a12b34f42ca38
MaercioMamedes <maerciomamedes@hotmail.com>
2023-05-06 14:18:06 -0300

    Finished Tutor viewset retargeting

--- core/views/TutorViewSet.py
+++ core/views/TutorViewSet.py
@@ -2,25 +2,23 @@ from rest_framework import viewsets
 from core.models import TutorProfile
 from core.serializers import TutorSerializer, TutorUpdateSerializer
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 
 
 class TutorViewSet(viewsets.ViewSet):
 
     http_method_names = ['get', 'post', 'put', 'patch', 'delete']
-
-    def get_queryset(self):
-        return TutorProfile.objects.get(pk=self.kwargs['pk'])
     
-    def get_serializer(self, **kwargs):
-        return TutorSerializer
+    def get_object(self, pk):
+        return get_object_or_404(TutorProfile, pk=pk)
 
     def list(self, request):
+        """Método para listar todos os objetos Tutores cadastrados"""
 
         queryset = TutorProfile.objects.all()
-
-        """Formatação da lista de tutores"""
-
+    
+        # Formatação da lista de tutores
         tutor_list = [
 
             {   
@@ -36,9 +34,9 @@ class TutorViewSet(viewsets.ViewSet):
         
         return Response(tutor_list)
 
-
     def create(self, request):
-            
+        """Método para criar um novo Tutor"""
+
         serializer_data = TutorSerializer(data=request.data)
 
         if serializer_data.is_valid(raise_exception=True):
@@ -46,7 +44,6 @@ class TutorViewSet(viewsets.ViewSet):
             fullname = serializer_data.validated_data['fullname']
             email = serializer_data.validated_data['email']
             password = serializer_data.validated_data['password']
-
 
             """Criando usuário para aplicação"""
 
@@ -58,7 +55,6 @@ class TutorViewSet(viewsets.ViewSet):
             )
 
             user.set_password(password)
-
             user.save()
 
             """Criando perfil de Tutor"""
@@ -68,17 +64,16 @@ class TutorViewSet(viewsets.ViewSet):
                 fullname=fullname,
             )
 
-
-            
             return Response({
                 'fullname': tutor.fullname,
                 'email':tutor.user.email,
             },
-            status=200)
+            status=201)
 
     def retrieve(self, request, pk=None):
+        """Método para recuperar um tutor a partir do id"""
         
-        tutor = TutorProfile.objects.get(pk=pk)
+        tutor = self.get_object(pk)
                 
         return Response(
             {
@@ -92,14 +87,13 @@ class TutorViewSet(viewsets.ViewSet):
         )
 
     def update(self, request, pk=None):
-
         """Método HTTP para atualização total de recursos"""
-        serializer =TutorUpdateSerializer(data=request.data)
 
+        serializer =TutorUpdateSerializer(data=request.data)
 
         if serializer.is_valid(raise_exception=True):
 
-            tutor = TutorProfile.objects.get(pk=pk)
+            tutor = self.get_object(pk)
 
             data = {}
             
@@ -129,14 +123,12 @@ class TutorViewSet(viewsets.ViewSet):
 
         serializer = TutorUpdateSerializer(data=request.data)
 
-
         if serializer.is_valid(raise_exception=True):
 
-            tutor = TutorProfile.objects.get(pk=pk)
+            tutor = self.get_object(pk)
             data = {}
 
             for attr in serializer.validated_data.keys():
-
                 data[attr] = serializer.validated_data[attr]
                 
             self._update_tutor(tutor, data)
@@ -144,19 +136,17 @@ class TutorViewSet(viewsets.ViewSet):
             return Response('dados atualizados com sucesso')
 
     def destroy(self, request, pk=None):
-        print("passsou aqui")
+        """Método para excluir um tutor"""
 
-        tutor = TutorProfile.objects.get(pk=pk)
-
+        tutor = self.get_object(pk)
         tutor.user.delete()
         tutor.delete()
-
         return Response('Usuário excluído com sucesso')
     
     
     def _update_tutor(self, tutor, data):
 
-            """Atualização parcial do usuário"""
+            """Atualização do tutor"""
 
             if 'fullname' in data.keys():
                 tutor.fullname = data['fullname']
